0009bb3457f4a925b59d9e226d0a04fb0b1bfcf7
Ishro Dev <dev@ishro.com>
2023-05-01 16:13:20 +0400

    BUG FIX

--- src/components/chat/chatController.js
+++ src/components/chat/chatController.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const chatModel = require("./chatModel");
 const authController = require("../auth/authController");
 const { validationResult } = require("express-validator");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 exports.getCategories = async (req, res) => {
   const errors = validationResult(req);
@@ -23,19 +28,29 @@ exports.createChat = async (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  let user = await authController.authorization(req, res);
-  let category = await chatModel.getCategoryById(req.body.category_id);
-  if (user && category) {
-    var body = {
-      user_id: user.user_id,
-      category_id: category.id,
-      question: req.body.question,
-      answer: req.body.question,
-    };
-    chat = await chatModel.createChat(body);
-    return res.status(201).json({
-      data: chat,
-    });
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: req.body.question,
+  });
+  if (completion.data.choices[0].text) {
+    let user = await authController.authorization(req, res);
+    let category = await chatModel.getCategoryById(req.body.category_id);
+    if (user && category) {
+      var body = {
+        user_id: user.user_id,
+        category_id: category.id,
+        question: req.body.question,
+        answer: completion.data.choices[0].text,
+      };
+      chat = await chatModel.createChat(body);
+      return res.status(201).json({
+        data: chat,
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ errors: [{ msg: "Error generating!!!!" }] });
+    }
   } else {
     return res.status(404).json({ errors: [{ msg: "Invalid request" }] });
   }
